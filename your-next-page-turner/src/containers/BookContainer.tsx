f25cd9eb41d35597b9b4ef838c73683acbee15c8
James Sharpe <jameswrightsharpe@gmail.com>
2020-01-20 16:14:45 -0500

    mvp searchbar

--- your-next-page-turner/src/containers/BookContainer.tsx
+++ your-next-page-turner/src/containers/BookContainer.tsx
@@ -10,15 +10,24 @@ interface Props {
 export default function BookContainer(props: Props): ReactElement {
   const [query, setQuery] = useState<string>("");
 
-  const renderBooks = () => {
-    return props.books.map((book, index) => {
+  const filterBooks = () => {
+    const { books } = props;
+    let filteredBooks = [...books];
+    if (query.length) {
+      filteredBooks = books.filter(book =>
+        ("" + book.titles).toLowerCase().includes(query.toLowerCase())
+      );
+    }
+
+    return filteredBooks.map((book, index) => {
       return <Book key={index} book={book} />;
     });
   };
+
   return (
     <div className="book-container">
       <SearchBar query={query} setQuery={setQuery} />
-      <ul>{renderBooks()}</ul>
+      <ul>{filterBooks()}</ul>
     </div>
   );
 }
