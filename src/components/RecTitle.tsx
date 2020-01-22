4ef50a264f991ae396fd1c2083852a2a57a8bd82
James Sharpe <jameswrightsharpe@gmail.com>
2020-01-22 16:46:49 -0500

    fixed header

--- src/components/RecTitle.tsx
+++ src/components/RecTitle.tsx
@@ -10,7 +10,8 @@ export default function RecTitle(props: Props): ReactElement {
   return (
     <div className="search">
       <div className="container rec-container">
-        <div className="rec-title">Books like {props.recTitle}</div>
+        <div className="rec-desc">Books recommendations like </div>
+        <div className="rec-title">{props.recTitle}</div>
         <div className="clear-rec-button" onClick={props.clearRecs}>
           clear recs
         </div>
